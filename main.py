211aa88982ab61ede464f8f89d900d446c2841ba
Andre Abas <dwedwe3300@gmail.com>
2022-11-24 19:07:41 +0800

    main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+
+print("1. ADD")
+print("2. SUBTRACT")
+print("3. MULTIPLY")
+print("4. DEVIDE")
+print("Select an operation to perform: ")
+
+operation = input()
+
+if operation == "1":
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number: ")
+    print("The sum is " + str(int(num1) + int(num2)))
+
+elif operation == "2":
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number: ")
+    print("The difference is " + str(int(num1) - int(num2)))
+
+elif operation == "3":
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number: ")
+    print("The product is " + str(int(num1) * int(num2)))
+
+elif operation == "4":
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number: ")
+    print("The result is " + str(int(num1) / int(num2)))
+else:
+    print("Invalid Entry")
